2b124a8dac3387a41fee24cc8dc3baca2479f5e6
Daghem Fanton <daghemfanton@gmail.com>
2026-02-09 11:09:08 +0100

    Tolto script di test per api

--- test_requests.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "content": "Qual è la capitale della Francia?"}
-response = requests.post("http://localhost:8000/", headers=headers)
-print(response.json())
